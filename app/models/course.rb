b1b681167373b9c6687a024ad1ec5f9b3bc416b8
sjoulbak <remco_meinen@hotmail.com>
2016-01-19 19:50:31 +0100

    Fixed many to many relationship with distinct

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ActiveRecord::Base
   belongs_to :soortcourse
 
   has_many :course_users, dependent: :destroy
-  has_many :users, through: :course_users
+  has_many :users, -> { distinct }, through: :course_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :course_users, dependent: :destroy
-  has_many :courses, through: :course_users
-  
+  has_many :courses, -> { distinct }, through: :course_users
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
