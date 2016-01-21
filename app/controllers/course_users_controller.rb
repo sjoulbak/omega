9e387676005bca79e09bc54c6e93b7c15f4aef23
sjoulbak <remco_meinen@hotmail.com>
2016-01-21 16:41:25 +0100

    Added custom theme

--- app/controllers/course_users_controller.rb
+++ app/controllers/course_users_controller.rb
@@ -3,10 +3,10 @@ class CourseUsersController < ApplicationController
     @course = Course.find course_users_params[:course_id]
     @course_user = CourseUser.new(course: @course, user: current_user)
     if @course_user.save
-      redirect_to course_path(@course)
+      redirect_to course_path(@course), notice: "Je bent ingeschreven voor de cursus"
     else
       CourseUser.where(course: @course, user: current_user).destroy_all
-      redirect_to course_path(@course)
+      redirect_to course_path(@course), alert: "Je bent uitgeschreven voor de cursus"
     end
   end
 
