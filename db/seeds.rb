4f59ded35ebb4aec4278f66d3df0e5b1b114d9df
sjoulbak <remco_meinen@hotmail.com>
2016-02-16 15:16:25 +0100

    Added seeds for Bart

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+if User.count == 0
+  puts 'Seeding the database...'
+  user1 = User.create(email: 'test_gebruiker@omega.nl', password: 'test_gebruiker', password_confirmation: 'test_gebruiker')
+  user2 = User.create(email: 'test_gebruiker2@omega.nl', password: 'test_gebruiker2', password_confirmation: 'test_gebruiker2')
+  User.create(email: 'test_medewerker@omega.nl', password: 'test_medewerker', password_confirmation: 'test_medewerker', admin: true)
+
+  soortcourse = Soortcourse.create(cursussoort: 'Beginnerscursus', prijs: 10)
+
+  course1 = Course.create(begindatum: DateTime.now, einddatum: DateTime.tomorrow, soortcourse: soortcourse)
+  course2 = Course.create(begindatum: DateTime.tomorrow, einddatum: DateTime.now + 7.days, soortcourse: soortcourse)
+
+  user1.course_users.create(course: course1)
+  user1.course_users.create(course: course2)
+  user2.course_users.create(course: course1)
+  puts 'Seed complete.'
+else
+  puts 'Error, cannot seed the database. Users already exists.'
+end
